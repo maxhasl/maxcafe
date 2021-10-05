257a416146e6ebc44856a267e1c7c94580b60257
maxhasl <maxhasl@ukr.net>
2021-10-05 15:06:18 +0300

    add base reducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import './index.css';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import { restaurants } from './fixtures';
+import store from './redux/store';
+
+//DEV ONLY!!!
+window.store = store;
 
 ReactDOM.render(
   <App restaurants={restaurants} />,
