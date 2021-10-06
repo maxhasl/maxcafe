356e74dd25ffd816927b5a6fd060448b1a2bd707
maxhasl <maxhasl@ukr.net>
2021-10-06 12:47:27 +0300

    move restaurants to redux

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,4 @@
 import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
 import Restaurants from '../restaurants';
 import Header from '../header';
 
@@ -8,14 +7,10 @@ class App extends PureComponent {
     return (
       <div>
         <Header />
-        <Restaurants restaurants={this.props.restaurants} />
+        <Restaurants />
       </div>
     );
   }
 }
-
-App.propTypes = {
-  restaurants: PropTypes.array,
-};
 
 export default App;
