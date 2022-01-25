9e71932cbdd5b5f9ce3d2736d39ca71de93a450c
maxhasl <maxhasl@ukr.net>
2022-01-25 12:27:18 +0200

    add redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
 import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from './middleware/logger';
 import generateId from './middleware/generateId';
 import apiCall from './middleware/apiCall';
 import reducer from './reducer';
 
-const middleware = [apiCall, generateId, logger];
+const middleware = [thunk, apiCall, generateId, logger];
 
 export default createStore(
   reducer,
